f0646332401460989576ae0c09dbfb15c0cafed0
SanchelliosProg <vasyania2009@gmail.com>
2017-03-04 07:38:32 +0300

    - ex.8

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.TestApp;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,15 +14,22 @@ public class BaseTest {
     protected static WebDriver driver;
     protected static WebDriverWait wait;
     protected String ADMIN_URL = "http://localhost/litecart/admin/";
+    protected String MAIN_URL_EN = "http://localhost/litecart/en/";
     @BeforeClass
     public static void setUp(){
         driver = TestApp.getInstance().getWebDriver();
         driver.manage().timeouts().implicitlyWait(250, TimeUnit.MILLISECONDS);
-
     }
 
     @AfterClass
     public static void tearDown(){
         driver.quit();
+    }
+
+    protected void log(Object ... objects) {
+        for (Object obj : objects) {
+            System.out.print(String.valueOf(obj) + " ");
+        }
+        System.out.println();
     }
 }
--- src/test/java/StickerTest.java
+++ src/test/java/StickerTest.java
@@ -1,5 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+
 /**
  * Created by Александр on 04.03.2017.
  */
-public class StickerTest {
+public class StickerTest extends BaseTest{
+
+    @Before
+    public void navigateToMainPage(){
+        driver.get(MAIN_URL_EN);
+    }
+
+    @Test
+    public void checkPopularStickersTest(){
+        String mostPopularLocator = "div#box-most-popular div.content > ul > li";
+        List<WebElement> webElements = driver.findElements(By.cssSelector(mostPopularLocator));
+        log("There are " + webElements.size() + " elements found by locator: " + mostPopularLocator);
+        assertStickersArePresented(webElements);
+    }
+
+    @Test
+    public void checkCampaignsStickersTest(){
+        String campaignsLocator = "div#box-campaigns div.content > ul > li";
+        List<WebElement> webElements = driver.findElements(By.cssSelector(campaignsLocator));
+        log("There are " + webElements.size() + " elements found by locator: " + campaignsLocator);
+        assertStickersArePresented(webElements);
+    }
+
+    @Test
+    public void checkLatestStickersTest() {
+        String latestLocator = "div#box-latest-products div.content > ul > li";
+        List<WebElement> webElements = driver.findElements(By.cssSelector(latestLocator));
+        log("There are " + webElements.size() + " elements found by locator: " + latestLocator);
+        assertStickersArePresented(webElements);
+    }
+
+    private void assertStickersArePresented(List<WebElement> webElements) {
+        for (WebElement webElement : webElements){
+            assertThat(webElement.findElement(By.cssSelector("div.image-wrapper div.sticker")).isDisplayed(), is(true));
+        }
+    }
 }
